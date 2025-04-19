9eb395cc0230e653fd45536f113a7ea7257078ad
Ohad <ohad9002@gmail.com>
2025-04-20 01:15:33 +0300

    Fix React import issues and update JSX runtime

--- frontend/src/components/PopcornLoader.tsx
+++ frontend/src/components/PopcornLoader.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./PopcornLoader.css";
 
 const PopcornLoader = () => {
--- frontend/src/pages/Preferences.tsx
+++ frontend/src/pages/Preferences.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import useAuthStore from "../store/authStore.ts";
 import ButtonRating from "../components/ButtonRating.tsx";
